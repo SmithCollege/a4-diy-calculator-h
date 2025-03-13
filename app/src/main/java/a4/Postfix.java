39c7b9ec5e78244d8bf5c82231ecaf11016ac45e
haykeys <hma@smith.edu>
2025-03-13 21:02:16 +0000

    initial commit- postfix class

--- app/src/main/java/a4/Postfix.java
+++ app/src/main/java/a4/Postfix.java
@@ -5,6 +5,51 @@ import java.util.ArrayDeque;
 public class Postfix {
 
     public static Double postfix(ArrayDeque<Object> tokens) {
+        Deque<double> stack = new ArrayDeque<>(); // stack
+
+        while (!=tokens.isEmpty()) { //token processed until the queue is empty
+            Object token = removeFirst(); //removeFirst from queue
+        }
+
+        if (token instanceof double) {  //differentiate Doubles from operators (Character)
+            stack.push((Double) token); //add to stack
+        } else if (token instanceof Character) {
+            if (stack.size() < 2) {
+                throw new IllegalArgumentException("Not enough numbers to operate.")
+            }
+        }
+
+        double a = stack.removeFirst();
+        double b = stack.removeFirst();
+        char operator = (Character) token;
+        double result;
+
+        switch(operator) {
+            case '+':
+                result: 'a + b';
+                break;
+            case '-':
+                result: 'b - a';
+                break;
+            case '/':
+                if(b == 0.0) {
+                    throw new IllegalArgumentException("Can't divide by zero.");
+                }
+                result: 'a / b';
+                break;     
+            case '*':
+                result: 'a * b';
+                break;
+            case '^':
+                result: 'Math.pow(a, b)';
+                break;
+            default:
+                throw new IllegalArgumentException("Must input a valid operator.");
+        }
+
+
+
+        
     }
     
 }
